ad2ab434275e4344a345f7dda0adaea794d2501d
octo <allblvnk@gmail.com>
2020-09-29 23:49:45 +0100

    Import local files

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AudicaConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class OsuToAudica
+    {
+        public struct TimingPoint
+        {
+            public double beatTime;
+            public float ms;
+        }
+
+        public float MsToTick(float ms, List<TimingPoint> timingPoints)
+        {
+            //Timing points is assumed to be a list of timing points sorted in chronological order.
+            float tickTime = 0f;
+            int i = 0;
+
+            while (i + 1 < timingPoints.Count && ms > timingPoints[i + 1].ms)
+            {
+                tickTime += (timingPoints[i + 1].ms - timingPoints[i].ms) * 480f / (float)timingPoints[i].beatTime;
+                i++;
+            }
+            tickTime += (ms - timingPoints[i].ms) * 480f / (float)timingPoints[i].beatTime;
+            return tickTime;
+        }
+
+    }
+}
+
+
