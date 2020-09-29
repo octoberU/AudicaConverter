c112819f720e24872d21b8d93986dc44c68e3bc1
octo <allblvnk@gmail.com>
2020-09-30 00:56:03 +0100

    Divided the program into multiple classes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AudicaTools;
+using System;
 using System.Collections.Generic;
 
 namespace AudicaConverter
@@ -8,31 +9,16 @@ namespace AudicaConverter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
         }
     }
 
     public class OsuToAudica
     {
-        public struct TimingPoint
-        {
-            public double beatTime;
-            public float ms;
-        }
 
-        public float MsToTick(float ms, List<TimingPoint> timingPoints)
-        {
-            //Timing points is assumed to be a list of timing points sorted in chronological order.
-            float tickTime = 0f;
-            int i = 0;
 
-            while (i + 1 < timingPoints.Count && ms > timingPoints[i + 1].ms)
-            {
-                tickTime += (timingPoints[i + 1].ms - timingPoints[i].ms) * 480f / (float)timingPoints[i].beatTime;
-                i++;
-            }
-            tickTime += (ms - timingPoints[i].ms) * 480f / (float)timingPoints[i].beatTime;
-            return tickTime;
-        }
+
+
 
     }
 }
---
+++ osu! types/Utility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OsuTypes
+{
+    internal static class Utility
+    {
+        public static float MsToTick(float ms, List<TimingPoint> timingPoints)
+        {
+            //Timing points is assumed to be a list of timing points sorted in chronological order.
+            float tickTime = 0f;
+            int i = 0;
+
+            while (i + 1 < timingPoints.Count && ms > timingPoints[i + 1].ms)
+            {
+                tickTime += (timingPoints[i + 1].ms - timingPoints[i].ms) * 480f / (float)timingPoints[i].beatTime;
+                i++;
+            }
+            tickTime += (ms - timingPoints[i].ms) * 480f / (float)timingPoints[i].beatTime;
+            return tickTime;
+        }
+    }
+}
