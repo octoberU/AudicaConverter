1f44228388c494a664de3718f27bd65c33827a35
Zernys <run.haakon@gmail.com>
2020-10-22 23:58:48 +0200

    Fixed old map parsing

--- osu! types/osufile.cs
+++ osu! types/osufile.cs
@@ -234,9 +234,9 @@ namespace OsuTypes
             var split = line.Split(",");
             if (split.Length > 2)
             {
-                int effects = int.Parse(split[7]);
+                int effects = split.Length > 7 ? int.Parse(split[7]) : 0;
                 bool kiai = effects == 1 || effects == 5;
-                var timingPoint = new TimingPoint((int)float.Parse(split[0]), float.Parse(split[1]), kiai, !Convert.ToBoolean(int.Parse(split[6])));
+                var timingPoint = new TimingPoint((int)float.Parse(split[0]), float.Parse(split[1]), kiai, split.Length > 6 ? !Convert.ToBoolean(int.Parse(split[6])): false);
                 if (!timingPoint.inherited)
                 {
                     timingPoint.meter = int.Parse(split[2]);
